64ae15e5768e781b9ead0de0c46db8225e58394c
soujanyareddyM <121964113+soujanyareddyM@users.noreply.github.com>
2023-03-07 22:44:40 +0530

    Add files via upload

---
+++ _small_num_using_stack.py
@@ -0,0 +1,28 @@
+lass MinStack:
+    def _init_(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if not self.min_stack or x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+
+    def pop(self) -> None:
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+        
+min_stack = MinStack()
+min_stack.push(3)
+min_stack.push(5)
+min_stack.push(2)
+min_stack.push(1)
+print(min_stack.getMin())  
